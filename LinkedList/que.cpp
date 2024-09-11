cc98aeed093360cf6b9bc5f79b1d8b5ee4286542
simran417 <simranvohra417@email.com>
2024-09-11 22:07:30 +0530

    questions

--- LinkedList/que.cpp
+++ LinkedList/que.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<map>
 using namespace std;
 class node{
     public:
@@ -42,6 +43,30 @@ node* reverse(node* &head){
     head=prev;
 }
 
+// reverse k nodes:
+node* krev(node* head,int k){
+    // base case:
+    if(head==NULL){
+        return NULL;
+    }
+// for k nodes:
+node* next=NULL;
+node* curr=head;
+node* prev=NULL;
+int cnt=0;
+while(curr!=NULL && cnt<k){
+    next=curr->next;
+    curr->next=prev;
+    prev=curr;
+    curr=next;
+    cnt++;
+}
+if(next!=NULL){
+    head->next=krev(next,k);
+}
+return prev;
+}
+
 // to find length;
 int len(node* head){
     node* temp=head;
@@ -65,6 +90,39 @@ node* mid(node* head){
     }
     return temp;
 }
+// linked list is circular or not?
+bool iscircle(node* head){
+    if(head==NULL){
+        return true;
+    }
+    node* temp=head->next;
+    while(temp!=NULL&&temp!=head){
+        temp=temp->next;
+    }
+    if(temp==head){
+        return true;
+    }
+    if(temp==NULL){
+        return false;
+    }
+}
+
+// find or detect loop:
+bool dcir(node* head){
+    if(head==NULL){
+        return false;
+    }
+    map<node*,bool>visited;
+    node* temp=head;
+    while(temp!=NULL){
+        if(visited[temp]==true){
+            return true;
+        }
+        visited[temp]=true;
+        temp=temp->next;
+    }
+    return false;
+}
 
 // print:
 void print(node* head){
@@ -81,9 +139,24 @@ node* head=n1;
 insertN(head,9);
 insertN(head,8);
 insertN(head,7);
-// insertN(head,6);
-// reverse(head);
-node* ans=mid(head);
-cout<<ans->data<<endl;
+insertN(head,6);
 print(head);
+if(dcir(head)){
+    cout<<"true"<<endl;
+}else{
+    cout<<"false"<<endl;
+}
+
+// if(iscircle(head)){
+//     cout<<"yes"<<endl;
+// }
+// else{
+//     cout<<"no"<<endl;
+// }
+// // node* ans=krev(head,2);
+// head=ans;
+// reverse(head);
+// node* ans=mid(head);
+// cout<<ans->data<<endl;
+// print(head);
 }
