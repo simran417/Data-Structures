1cfc5eac55af395181440c2bd45b926149be1d40
simran417 <simranvohra417@email.com>
2025-01-07 20:50:10 +0530

    levelOrderTtraversal

---
+++ tree/levelOrderTraversal.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int d){
+        this->data=d;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+
+node* buildTree(node* root){
+    cout<<"enter the data: "<<endl;
+    int data;
+    cin>>data;
+    root=new node(data);
+    if(data==-1){
+        return NULL;
+    }
+    cout<<"enter the data for left tree: "<<data<<endl;
+    root->left=buildTree(root->left);
+    cout<<"enter the data for right tree: "<<data<<endl;
+    root->right=buildTree(root->right);
+
+    return root;
+}
+
+void levelOrderTraversal(node* root){
+    queue<node*>q;
+    q.push(root);
+    while(!q.empty()){
+        node* temp=q.front();
+        cout<<temp->data<<" ";
+        q.pop();
+
+        if(temp->left){
+            q.push(temp->left);
+        }
+        if(temp->right){
+            q.push(temp->right);
+        }
+    }
+}
+
+int main(){
+    node* root=NULL;
+    root= buildTree(root);
+    cout<<"level order traversal"<<endl;
+    levelOrderTraversal(root);
+}
