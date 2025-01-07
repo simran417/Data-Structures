194c85b835d911a02bf88aa95c87c4b2c13a9f91
simran417 <simranvohra417@email.com>
2025-01-07 21:13:27 +0530

    add

--- tree/levelOrderTraversal.cpp
+++ tree/levelOrderTraversal.cpp
@@ -33,16 +33,26 @@ node* buildTree(node* root){
 void levelOrderTraversal(node* root){
     queue<node*>q;
     q.push(root);
+    q.push(NULL);
     while(!q.empty()){
         node* temp=q.front();
-        cout<<temp->data<<" ";
+        // cout<<temp->data<<" ";
         q.pop();
 
-        if(temp->left){
-            q.push(temp->left);
+        if(temp==NULL){
+            cout<<endl;
+            if(!q.empty()){
+                q.push(NULL);
+            }
         }
-        if(temp->right){
-            q.push(temp->right);
+        else{
+            cout<<temp->data<<" ";
+            if(temp->left){
+                q.push(temp->left);
+            }
+            if(temp->right){
+                q.push(temp->right);
+            }
         }
     }
 }
