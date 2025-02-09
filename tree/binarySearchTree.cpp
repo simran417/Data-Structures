8b8f093371ee6671de14bdc519c6ad89d9dd183b
simran417 <simranvohra417@gmail.com>
2025-02-09 22:03:18 +0530

    add

--- tree/binarySearchTree.cpp
+++ tree/binarySearchTree.cpp
@@ -67,7 +67,7 @@ void levelOrderTraversal(Node* root){
 }
 
 // search in bst:
-bool search(Node* root,int x){
+bool search(Node* &root,int x){
 Node* temp=root;
 while(temp !=NULL){
     if(temp->data==x){
@@ -83,6 +83,68 @@ while(temp !=NULL){
 return false;
 }
 
+// find minimum value:
+Node* minval(Node* root){
+    Node* temp=root;
+    while(temp->left !=NULL){
+        temp=temp->left;
+    }
+    return temp;
+}
+
+// find maximum value:
+Node* maxval(Node* root){
+    Node* temp=root;
+    while(temp->right !=NULL){
+        temp=temp->right;
+    }
+    return temp;
+}
+
+// delete a node:
+Node* deleteNodebst(Node* root,int val){
+    // base case
+    if(root==NULL){
+        return root;
+    }
+    if(root->data==val){
+        // 0-child:
+        if(root->left==NULL && root->right ==NULL){
+            delete root;
+            return NULL;
+        }
+        // 1-child:
+        // left child:
+        if(root->left !=NULL && root->right==NULL){
+            Node* temp=root->left;
+            delete root;
+            return temp;
+        }
+        // right child:
+        if(root->left==NULL && root->right !=NULL){
+            Node* temp=root->right;
+            delete root;
+            return temp;
+        }
+        // 2 child:
+        if(root->left !=NULL && root->right !=NULL){
+            int mini=minval(root->right)->data;
+            root->data=mini;
+            root->right=deleteNodebst(root->right,mini);
+            return root;
+        }
+    }
+    else if(root->data>val){
+        // left part
+        root->left=deleteNodebst(root->left,val);
+        return root;
+    }
+    else{
+        root->right=deleteNodebst(root->right,val);
+        return root;
+    }
+}
+
 int main(){
     Node* root=NULL;
     cout<<"enter data "<<endl;
@@ -91,6 +153,21 @@ int main(){
     cout<<"printing the bst: "<<endl;
     levelOrderTraversal(root);
 
-    search(root,15);
+    int key=15;
+    if(search(root,key)){
+        cout<<key<<" is present"<<endl;
+    }else{
+        cout<<key<<" is not present"<<endl;
+    }
+
+  Node* ans=  minval(root);
+  cout<<"min val is: "<<ans->data<<endl;
+
+  Node* ans1=maxval(root);
+  cout<<"max val is: "<<ans1->data;
+
+  root=deleteNodebst(root,10);
+  cout<<"printing the bst: "<<endl;
+  levelOrderTraversal(root);
 
 }
