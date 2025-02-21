8a2ebc7eaaf6392b904316e9b0322f2d798d421a
simran417 <simranvohra417@gmail.com>
2025-02-21 22:51:02 +0530

    add

--- Tries/tries.cpp
+++ Tries/tries.cpp
@@ -15,6 +15,40 @@ class trieNode{
         isTerminal=false;
     }
 };
+
+class trie{
+    public:
+    trieNode* root;
+    // constructor:
+    trie(){
+        root=new trieNode('/0');
+    }
+
+    void insertutil(trieNode* root, string word){
+        // base case:
+        if(word.length()==0){
+            root->isTerminal=true;
+        }
+        // assumption: word will be in caps:
+        int index=word[0]-'A';
+        trieNode* child;
+        // present
+        if(root->children[index]!=NULL){
+            child=root->children[index];
+        }
+        else{
+            // absent:
+            child=new trieNode(word[0]);
+            root->children[index]=child;
+        }
+
+        // recursion:
+        insertutil(child,word.substr(1));
+    }
+    void insertWord(string word){
+        insertutil(root,word);
+    }
+};
 int main(){
-    
+    trie
 }
