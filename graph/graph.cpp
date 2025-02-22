8d5524b1d30689063fb1ba8d475b66d9f5e2f4b0
simran417 <simranvohra417@gmail.com>
2025-02-22 21:52:52 +0530

    graph

---
+++ graph/graph.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<unordered_map>
+#include<list>
+using namespace std;
+class graph{
+    public:
+    unordered_map<int, list<int>> adj;
+    void addEdge(int u, int v, bool direction){
+        // direction 0=undirected
+        // direction 1 = directed
+        // create an edge from u to v:
+        adj[u].push_back(v);
+        if(direction==0){
+            adj[v].push_back(u);
+        }
+    }
+    void printadj(){
+        for(auto i:adj){
+            cout<<i.first<<"->";
+            for(auto j: i.second){
+                cout<<j<<",";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+int main(){
+    int n;
+    cout<<"enter no. of node: ";
+    cin>>n;
+    int m;
+    cout<<"enter no. of edges: ";
+    cin>>m;
+    graph g;
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        g.addEdge(u,v,0);
+    }
+    g.printadj();
+}
